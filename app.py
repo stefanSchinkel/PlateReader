19f5bcf7e6b29bb119e0efe05dae06e7eeeeba99
Stefan Schinkel <mail@dreeg.org>
2019-04-17 23:14:09 +0200

    [app] add "memory" to web app

--- app.py
+++ app.py
@@ -3,13 +3,12 @@
 import os
 import uuid
 
-from flask import Flask, request, redirect, url_for, render_template, flash, send_from_directory
-from werkzeug.utils import secure_filename
+from jinja2 import Environment, FileSystemLoader
+from flask import Flask, request, redirect, render_template, flash, send_from_directory
 
 from lib.ExcelReader import ExcelReader
 
-UPLOAD_FOLDER = 'incoming'
-IMG_FOLDER = 'images'
+UPLOAD_DIR = 'results'
 ALLOWED_EXTENSIONS = set(['xls', 'xlsx'])
 
 app = Flask(__name__)
@@ -21,78 +20,126 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def render_images(er):
-    """renders all data sets to a random name"""
+
+def save_file(file):
+    """Save an uploaded file to a unique location
+
+    Args:
+        fil (obj): flask file object
+    """
+    save_dir = os.path.join(
+        app.instance_path,
+        UPLOAD_DIR,
+        str(uuid.uuid4()).split("-")[0]
+    )
+    os.makedirs(save_dir)
+    save_name = os.path.join(save_dir, "data.xlsx")
+    file.save(save_name)
+    return save_name
+
+def render_images(ex_reader, set_id):
+    """ Renders all images in an Excel document
+
+    Args:
+        ex_reader (ExcelReader): reader instance (processed)
+        set_id (str): unique identifier of result set
+    """
+
+    # create dir for images
+    save_dir = os.path.join(app.config['UPLOAD_DIR'], set_id, "images")
+    os.makedirs(save_dir)
 
     urls = {}
-    for name, sheet in er.sheets.items():
+    for name, sheet in ex_reader.sheets.items():
         urls[name] = {}
         for marker, data in sheet.items():
             # savename (local) and urls for serving
             _id = str(uuid.uuid4()).split("-")[0] + ".png"
-            savename = os.path.join(app.config['IMG_FOLDER'], _id)
-            urls[name][marker] = "/{}/{}".format(IMG_FOLDER, _id)
+            savename = os.path.join(save_dir, _id)
+            urls[name][marker] = "/results/{}/images/{}".format(set_id, _id)
             # default title and rendereing
             title = "{}:{}".format(name, marker)
             data.plot(title=title, savename=savename)
 
     return urls
 
-def process_file(filename):
+def process_file(filename, set_id):
     """Process the excel file and return proper data
 
     Args:
         filename (str): file to process
+        set_id (str): unique identifier of result set
     """
-    er = ExcelReader(filename)
-    er.main()
-    img = render_images(er)
+    # instantiate Reader and process
+    ex_reader = ExcelReader(filename)
+    ex_reader.main()
 
-    return render_template('view.html', sheets=er.sheets, images=img)
+    # render all images
+    img = render_images(ex_reader, set_id)
 
-@app.route('/', methods=['GET', 'POST'])
+    # render an index page
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('view.html')
+    output = template.render(sheets=ex_reader.sheets, images=img)
+
+    # and save the result to unique dir
+    index = filename.replace("data.xlsx", "index.html")
+    with open(index, "w") as f_handle:
+        f_handle.write(output)
+
+    return True
+##
+## Endpoint mapping
+##
+@app.route('/', methods=['GET'])
+def show_results():
+    """List all file and show upload button"""
+    dirs = next(os.walk(app.config['UPLOAD_DIR']))[1]
+    try:
+        dirs.remove("images")
+    except ValueError:
+        pass
+
+    return render_template('index.html', dirs=dirs)
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    # Get jus presents the upload dialog
-    if request.method == 'GET':
-        return render_template('index.html')
+    """Endpoint for uploading and processing files"""
 
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            save_name = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(save_name)
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect('/')
 
-        return process_file(save_name)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = save_file(file)
+        set_id = filename.split("/")[-2]
+
+    # to all the rendering and stuff
+    process_file(filename, set_id)
+
+    return redirect("/results/{}/index.html".format(set_id))
 
 # we need a route to serve the computed images
-@app.route('/images/<path:filepath>')
-def data(filepath):
-    fname = filepath.split('/')[-1]
-    return send_from_directory(app.config['IMG_FOLDER'], fname)
-
+@app.route('/results/<path:filepath>')
+def serve(filepath):
+    """Serving of static files"""
+    return send_from_directory(app.config['UPLOAD_DIR'], filepath)
 
 if __name__ == '__main__':
-    """main wrapper"""
-    app.secret_key = b'_53k#-Lldk34sk++sl'
-    app.config['UPLOAD_FOLDER'] = os.path.join(app.instance_path, UPLOAD_FOLDER)
-    app.config['IMG_FOLDER'] = os.path.join(app.instance_path, IMG_FOLDER)
 
+    app.secret_key = b'_53k#-Lldk34sk++sl'
+    app.config['UPLOAD_DIR'] = os.path.join(app.instance_path, UPLOAD_DIR)
+
+    # make sure upload dir exists
     try:
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    except OSError:
-        pass
-    try:
-        os.makedirs(app.config['IMG_FOLDER'])
+        os.makedirs(app.config["UPLOAD_DIR"])
     except OSError:
         pass
 
